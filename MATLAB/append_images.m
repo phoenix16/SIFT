63dc0892d0eed9d7dfb1909be976664b4c59bb25
Prakriti C <c.prakriti@gmail.com>
2018-01-31 12:05:05 -0800

    Update append_images.m

--- MATLAB/append_images.m
+++ MATLAB/append_images.m
@@ -1,40 +1,40 @@
-%/*
-% * Developer : Prakriti Chintalapoodi - c.prakriti@gmail.com 
-%*/
+%
+%  Developer : Prakriti Chintalapoodi - c.prakriti@gmail.com 
+%
 
 function appended = append_images(mode, varargin)
-% Function to append images Left-Right or Top-Bottom
-%       mode = 'LR' or 'TB' for left-right or top-bottom concatenation
-% Rows (or cols) at the bottom (or right) of the smaller images are zero padded
+    % Function to append images Left-Right or Top-Bottom
+    %       mode = 'LR' or 'TB' for left-right or top-bottom concatenation
+    % Rows (or cols) at the bottom (or right) of the smaller images are zero padded
 
-% Create a copy of input images before messing with them 
-% (else would be pass by reference)
-images = cell(1,nargin); images = varargin; 
+    % Create a copy of input images before messing with them 
+    % (else would be pass by reference)
+    images = cell(1,nargin); images = varargin; 
 
-% dim_array corresponds to rows if 'LR' and cols if 'TB'
-dim_array = zeros(1,nargin-1);
-for n = 1:(nargin-1)
-    if strcmp(mode,'LR')
-        dim_array(n) = size(images{n},1);
-    else
-        dim_array(n) = size(images{n},2);
-    end   
-end
-max_dim = max(dim_array);
-appended = [];
-
-for n = 1:nargin-1
-    switch mode 
-        case 'LR'  % Pad zeros at the bottom of smaller images
-            if size(images{n},1) < max_dim
-                images{n}(max_dim,1) = 0;
-            end
-            appended = [appended   images{n}];
-        case 'TB'  % Pad zeros at the right of smaller images
-            if size(images{n},2) < max_dim
-                images{n}(1,max_dim) = 0;
-            end   
-            appended = [appended ; images{n}];   
+    % dim_array corresponds to rows if 'LR' and cols if 'TB'
+    dim_array = zeros(1,nargin-1);
+    for n = 1:(nargin-1)
+        if strcmp(mode,'LR')
+            dim_array(n) = size(images{n},1);
+        else
+            dim_array(n) = size(images{n},2);
+        end   
     end
-end
+    max_dim = max(dim_array);
+    appended = [];
+
+    for n = 1:nargin-1
+        switch mode 
+            case 'LR'  % Pad zeros at the bottom of smaller images
+                if size(images{n},1) < max_dim
+                    images{n}(max_dim,1) = 0;
+                end
+                appended = [appended   images{n}];
+            case 'TB'  % Pad zeros at the right of smaller images
+                if size(images{n},2) < max_dim
+                    images{n}(1,max_dim) = 0;
+                end   
+                appended = [appended ; images{n}];   
+        end
+    end
 end
